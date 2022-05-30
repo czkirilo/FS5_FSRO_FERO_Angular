9c5f8c47e74c6ee8de372d01a776bbf7c7651d10
czkirilo <caiquez.kirilo@hotmail.com>
2022-05-30 20:44:57 -0300

    up

---
+++ ER2/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { ContatoComponent } from './views/contato/contato.component';
+import { HeaderComponent } from './views/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContatoComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ER2/src/app/views/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Noticia } from 'src/app/models/noticia';
+import { User } from 'src/app/models/user';
+import { NoticiasService } from 'src/app/services/noticias.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private noticiaService: NoticiasService) { }
+
+  listaNoticias = [] as Noticia[]
+
+  ngOnInit(): void {
+    this.carregarNoticias()
+  }
+
+carregarNoticias(){
+  this.noticiaService.getNoticias().subscribe( (noticiasRecebidas: Noticia[]) => {
+    this.listaNoticias = noticiasRecebidas;
+    console.log(this.listaNoticias);
+  }
+  
+  )
+}
+
+}
